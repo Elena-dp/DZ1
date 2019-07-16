94b60eb2c0fa34ade012b940a00e98b43b7e8424
Elena <elenadp@yandex.ru>
2019-07-16 14:28:27 +0300

    оформлено в самостоятельные методы

--- dz.py
+++ dz.py
@@ -12,19 +12,21 @@ class Dz1(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_dz1(self):
-        wd = self.wd
-        #open home page
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        #login
+
+    def login(self, wd):
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        #open groups page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
-        #create new group
+
+    def create_group(self, wd):
+        # create new group
         wd.find_element_by_name("new").click()
         #fill group form
         wd.find_element_by_name("group_name").click()
@@ -36,10 +38,21 @@ class Dz1(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("dddddd")
         #submit group creation
         wd.find_element_by_name("submit").click()
-        #return groups page
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
-        #logout
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_dz1(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
